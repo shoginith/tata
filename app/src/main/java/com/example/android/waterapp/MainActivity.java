1c61ca6cd8f84bb7445987742d534d3dd843a38e
shoginith <shoginith@gmail.com>
2020-03-17 12:02:05 +0100

    initial commit

---
+++ app/src/main/java/com/example/android/waterapp/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.android.waterapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    public static final String USERNAME_KEY =
+            "com.example.android.twoactivities.extra.USERNAME_KEY";
+    public static final String PASSWORD_KEY =
+            "com.example.android.twoactivities.extra.PASSWORD_KEY";
+    private EditText mMainUsername;
+    private EditText mMainPassword;
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        //%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        mMainUsername = findViewById(R.id.editText_username);
+        mMainPassword = findViewById(R.id.editText_password);
+        //%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+        //FloatingActionButton fab = findViewById(R.id.fab);
+/*        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    public void verifycredentials(View view) {
+        // insérer ici le code qui sert à vérifier les données d'identification entrées par l'utilisateur
+        boolean credentials;
+        credentials = false;
+        String username = mMainUsername.getText().toString();
+        String password = mMainPassword.getText().toString();
+
+        if(true){
+                   credentials=true; // vérifications des données, si c'est juste, on met credentials à 1
+                }
+        if (credentials == true){
+            Intent intent = new Intent(this, OverviewActivity.class);
+            startActivity(intent);
+        }
+    }
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+}
